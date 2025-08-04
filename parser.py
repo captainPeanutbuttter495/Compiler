32bbda661e5dc62028660f80e62222765c561513
captainPeanutbuttter495 <matthew.garcia.081@my.csun.edu>
2025-08-04 12:26:10 -0700

    added expect() helper function to validate and consume tokens during parsing

--- parser.py
+++ parser.py
@@ -15,3 +15,38 @@ def parse_program(tokens):
 
     # wrap function node in a Program node and return the AST root
     return Program(function_node)
+
+# parse C program structure
+# build a function node and a body
+# generate formal grammer line by line
+def parse_function(tokens):
+    expect(tokens, "KEYWORD", "int")
+    name_token = expect(tokens, "IDENT")
+    expect(tokens, "LPAREN")
+    expect(tokens, "KEYWORD", "void")
+    expect(tokens, "RPAREN")
+    expect(tokens, "LBRACE")
+
+    body = parse_statement(tokens)
+
+    expect(tokens, "RBRACE")
+
+    return Function(name_token.value, body)
+
+def expect(tokens, expected_type, expected_value=None):
+    # checks that a token exists
+    if not tokens:
+        raise SyntaxError(f"Unexpected end of input")
+
+    token = tokens[0]
+
+    # checks its type and match expectatoin
+    if token.type != expected_type:
+        raise SyntaxError(f"Expected {expected_type}, but got {token.type}")
+
+    if expected_value is not None and token.value != expected_value:
+        raise SyntaxError(f"Expected {expected_value!r}, but got {token.value!r}")
+
+
+    # removes token from the list and returns it
+    return tokens.pop(0)
